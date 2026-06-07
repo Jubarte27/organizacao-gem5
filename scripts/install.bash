a5b8de2d01d1988a79c0e5621ccaf42bc5abc763
Jubarte <mmcezar@inf.ufrgs.br>
2026-06-07 09:49:43 -0300

    docker init

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,25 +2,21 @@
 
 main() {
     set_log_depth 0
-
-    if ! pyenv --version > /dev/null 2>&1 ; then log_error "no pyenv $?"; fi
-    pyenv install --skip-existing 3.12
-    pyenv local 3.12
-
-    GEM_5_DIR="$PROJECT_DIR/gem5"
-    VENV_DIR="$PROJECT_DIR/.venv"
-    MAX_THREAD="$(nproc --ignore 2)"
-
-    if ! [ -d "$VENV_DIR" ]; then python3 -m venv "$VENV_DIR"; fi
-    if ! [ -d "$GEM_5_DIR" ]; then git clone https://github.com/gem5/gem5 "$GEM_5_DIR"; fi
-
-    # shellcheck source=../.venv/bin/activate
-    source "$VENV_DIR/bin/activate"
-    pip install -r "$PROJECT_DIR/requirements.txt"
-
-    ensure cd "$GEM_5_DIR"
-    scons build/ALL/gem5.opt -j "$MAX_THREAD"
+    
+    build_if_not_exists $IMAGE_NAME gem5
 }
+
+build_if_not_exists() {
+    if [[ "$(docker images -q "$1" 2> /dev/null)" == "" ]]; then
+        echo "Image $1 does not exist. Building..."
+        docker build --build-arg MAX_THREAD="$(nproc --ignore 2)" --force-rm --tag "$1" "$2"
+        # make -C "$PROJECT_DIR" build
+    else
+        echo "Image $1 already exists. Skipping build."
+    fi
+}
+
+run_docker()        { docker run --rm -v "$(pwd):/data" -w /data $IMAGE_NAME "${@}";}
 
 _setConfigArgs() {
     while [ "${1:-}" != '' ]; do
@@ -41,10 +37,9 @@ _setConfigArgs() {
 
 
 set_env() {
-    :
+    IMAGE_NAME=orgb:latest
 }
 
-PROJECT_DIR=..
 SCRIPT_DIR=$(dirname "$(readlink -e "${BASH_SOURCE[0]}")") && source "$SCRIPT_DIR/util.bash"
 set_env
 _setConfigArgs "$@"
