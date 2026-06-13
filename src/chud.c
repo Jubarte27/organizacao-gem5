62f2e6620497a9eb8b32abfdbcd01d1ac7f5afda
Jubarte <mmcezar@inf.ufrgs.br>
2026-06-13 11:40:15 -0300

    meio melhor pra memoria

--- src/chud.c
+++ src/chud.c
@@ -4,8 +4,24 @@
 #include <gmp.h>
 #include <stdint.h>
 
-void chud(int digits) {
-    int N = (digits * 3.321928) / 64 + 3; // conta de padeiro, funciona pra poucos (3mi) digitos
+// economiza 1 if por iteracao
+// everything fits in uint64_t as long as k < 219,000 (+-)
+#define gambiarra do { \
+        uint64_t num = 24 * (6 * k - 5) * (2 * k - 1) * (6 * k - 1); \
+        uint64_t hehe = k * k * k; \
+        uint64_t C = 262537412640768000;\
+        uint64_t term_mult = 13591409 + 545140134 * k; \
+        mpn_mul_1(P, P, N, num); \
+        mpn_divrem_1(term, 0, P, N, hehe); \
+        mpn_divrem_1(P, 0, term, N, C); \
+        mpn_mul_1(term, P, N, term_mult); \
+    } while (0)
+
+
+void chud(uint32_t digits) {
+    float floaty_bits = digits * log2(10);
+    uint32_t bits = floaty_bits + 5;
+    int N = floaty_bits / 64 + 3;
 
     mp_limb_t P[N];
     mp_limb_t sum[N];
@@ -18,45 +34,44 @@ void chud(int digits) {
 
     uint64_t iterations = digits / 14 + 1;
     for (uint64_t k = 1; k <= iterations; k++) {
-        // everything fits in uint64_t as long as k < 219,000 (+-)
-        uint64_t num = 24 * (6 * k - 5) * (2 * k - 1) * (6 * k - 1);
-        uint64_t hehe = k * k * k;
-        uint64_t C = 262537412640768000; // 640320^3
-        uint64_t term_mult = 13591409 + 545140134 * k;
+        gambiarra;
+        mpn_sub_n(sum, sum, term, N);
 
-        mpn_mul_1(P, P, N, num);            // 1. P = P * num
-        mpn_divrem_1(term, 0, P, N, hehe);  // 2. term = P / hehe
-        mpn_divrem_1(P, 0, term, N, C);     // 3. P = term / C
-        mpn_mul_1(term, P, N, term_mult);   // 4. term = P * term_mult
+        k++;
+        if (k > iterations) break; 
 
-        if (k % 2 == 0) {
-            mpn_add_n(sum, sum, term, N);
-        } else {
-            mpn_sub_n(sum, sum, term, N);
-        }
+        gambiarra;
+        mpn_add_n(sum, sum, term, N);
     }
 
     mpz_t z_sum;
-    mpz_roinit_n(z_sum, sum, N); // Bind the stack array to an mpz safely
+    mpz_roinit_n(z_sum, sum, N);
 
-    unsigned int bits = digits * log2(10) + 5;
-    mpf_t f_sum, pi, constant;
+    mpf_t f_sum;
     mpf_init2(f_sum, bits);
-    mpf_init2(pi, bits);
-    mpf_init2(constant, bits);
-
     mpf_set_z(f_sum, z_sum);
     mpf_div_2exp(f_sum, f_sum, 64 * (N - 1));
 
+    mpf_t constant;
+    mpf_init2(constant, bits);
     mpf_sqrt_ui(constant, 10005);
     mpf_div_ui(constant, constant, 4270934400);
-
     mpf_mul(f_sum, f_sum, constant);
+    mpf_clear(constant);
+
+    mpf_t pi;
+    mpf_init2(pi, bits);
     mpf_ui_div(pi, 1, f_sum);
+    mpf_clear(f_sum);
 
-    gmp_printf("pi: %.*Ff\n", digits, pi);
+    //1 extra porque a desgraça me arredonda
+    char pi_size = 1 + digits + sizeof("3."); // 3.{$digits}\0
+    char pi_str[pi_size]; 
+    gmp_snprintf(pi_str, pi_size, "%.*Ff", digits + 1, pi);
+    mpf_clear(pi);
 
-    mpf_clears(f_sum, pi, constant, NULL);
+    pi_str[pi_size - 2] = '\0';
+    printf("pi: %s\n", pi_str);
 }
 
 
