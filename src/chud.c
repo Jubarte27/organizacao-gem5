ff9444922154b70ec151c96c049e23bafb2c0944
Jubarte <mmcezar@inf.ufrgs.br>
2026-06-13 10:41:23 -0300

    menos ponteiro

--- src/chud.c
+++ src/chud.c
@@ -2,62 +2,61 @@
 #include <stdlib.h>
 #include <math.h>
 #include <gmp.h>
-
-#define mp_init(type, args...) mp##type##_t args; mp##type##_inits(args, NULL)
+#include <stdint.h>
 
 void chud(int digits) {
-    unsigned int bits = digits * log2(10) + 5;
-    mpf_t pi, temp, constant;
-    mpf_init2(pi, bits);
-    mpf_init2(temp, bits);
-    mpf_init2(constant, bits);
-    mp_init(z, fac6, fac3, fac, top, bottom);
-    mp_init(q, term);
+    int N = (digits * 3.321928) / 64 + 3; // conta de padeiro, funciona pra poucos (3mi) digitos
 
-    int iterations = digits / 14;
+    mp_limb_t P[N];
+    mp_limb_t sum[N];
+    mp_limb_t term[N];
+
+    for (int i = 0; i < N - 1; i++) {P[i] = 0; sum[i] = 0;}
+
+    P[N - 1] = 1;
+    sum[N - 1] = 13591409;
+
+    uint64_t iterations = digits / 14 + 1;
+    for (uint64_t k = 1; k <= iterations; k++) {
+        // everything fits in uint64_t as long as k < 219,000 (+-)
+        uint64_t num = 24 * (6 * k - 5) * (2 * k - 1) * (6 * k - 1);
+        uint64_t hehe = k * k * k;
+        uint64_t C = 262537412640768000; // 640320^3
+        uint64_t term_mult = 13591409 + 545140134 * k;
+
+        mpn_mul_1(P, P, N, num);            // 1. P = P * num
+        mpn_divrem_1(term, 0, P, N, hehe);  // 2. term = P / hehe
+        mpn_divrem_1(P, 0, term, N, C);     // 3. P = term / C
+        mpn_mul_1(term, P, N, term_mult);   // 4. term = P * term_mult
+
+        if (k % 2 == 0) {
+            mpn_add_n(sum, sum, term, N);
+        } else {
+            mpn_sub_n(sum, sum, term, N);
+        }
+    }
+
+    mpz_t z_sum;
+    mpz_roinit_n(z_sum, sum, N); // Bind the stack array to an mpz safely
+
+    unsigned int bits = digits * log2(10) + 5;
+    mpf_t f_sum, pi, constant;
+    mpf_init2(f_sum, bits);
+    mpf_init2(pi, bits);
+    mpf_init2(constant, bits);
+
+    mpf_set_z(f_sum, z_sum);
+    mpf_div_2exp(f_sum, f_sum, 64 * (N - 1));
 
     mpf_sqrt_ui(constant, 10005);
     mpf_div_ui(constant, constant, 4270934400);
 
-
-    for (int k = 0; k < iterations; k++) {
-        mpz_fac_ui(fac6, 6*k);
-        mpz_fac_ui(fac3, 3*k);
-
-        mpz_fac_ui(fac, k);
-        mpz_pow_ui(fac, fac, 3);
-
-        mpz_set_ui(top, 545140134);
-        mpz_mul_ui(top, top, k);
-        mpz_add_ui(top, top, 13591409);
-
-        mpz_mul(top, top, fac6);
-
-        mpz_ui_pow_ui(bottom, 640320, 3*k);
-        mpz_mul(bottom, bottom, fac3);
-        mpz_mul(bottom, bottom, fac);
-
-        mpq_set_num(term, top);
-        mpq_set_den(term, bottom);
-
-        mpf_set_q(temp, term);
-
-        if (k % 2 == 0) {
-            mpf_add(pi, pi, temp);
-        } else {
-            mpf_sub(pi, pi, temp);
-        }
-    }
-
-    mpf_mul(pi, pi, constant);
-    mpf_ui_div(pi, 1, pi);
+    mpf_mul(f_sum, f_sum, constant);
+    mpf_ui_div(pi, 1, f_sum);
 
     gmp_printf("pi: %.*Ff\n", digits, pi);
 
-    mpf_clears(pi, temp, constant, NULL);
-    mpz_clears(fac6, fac3, fac, top, bottom, NULL);
-    mpq_clears(term, NULL);
-
+    mpf_clears(f_sum, pi, constant, NULL);
 }
 
 
