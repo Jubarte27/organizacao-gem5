0744383aa2faf65d19402f147be0cf7f8b284e60
Jubarte <mmcezar@inf.ufrgs.br>
2026-06-13 12:10:38 -0300

    sem float

--- src/chud.c
+++ src/chud.c
@@ -1,23 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 #include <math.h>
 #include <gmp.h>
 #include <stdint.h>
 
-// economiza 1 if por iteracao
-// everything fits in uint64_t as long as k < 219,000 (+-)
-#define gambiarra do { \
-        uint64_t num = 24 * (6 * k - 5) * (2 * k - 1) * (6 * k - 1); \
-        uint64_t hehe = k * k * k; \
-        uint64_t C = 262537412640768000;\
-        uint64_t term_mult = 13591409 + 545140134 * k; \
-        mpn_mul_1(P, P, N, num); \
-        mpn_divrem_1(term, 0, P, N, hehe); \
-        mpn_divrem_1(P, 0, term, N, C); \
-        mpn_mul_1(term, P, N, term_mult); \
-    } while (0)
 
-
+// funciona até uns k = 219 mil, depois disso tem que usar toda a gmp mesmo 
 void chud(uint32_t digits) {
     float floaty_bits = digits * log2(10);
     uint32_t bits = floaty_bits + 5;
@@ -34,46 +24,49 @@ void chud(uint32_t digits) {
 
     uint64_t iterations = digits / 14 + 1;
     for (uint64_t k = 1; k <= iterations; k++) {
-        gambiarra;
-        mpn_sub_n(sum, sum, term, N);
+        uint64_t num       = 24 * (6 * k - 5) * (2 * k - 1) * (6 * k - 1);
+        uint64_t den1      = k * k * k;
+        uint64_t C         = 262537412640768000; 
+        uint64_t term_mult = 13591409 + 545140134 * k;
 
-        k++;
-        if (k > iterations) break; 
+        mpn_mul_1(P, P, N, num);
+        mpn_divrem_1(term, 0, P, N, den1);
+        mpn_divrem_1(P, 0, term, N, C);
+        mpn_mul_1(term, P, N, term_mult);
 
-        gambiarra;
-        mpn_add_n(sum, sum, term, N);
+        if (k % 2 == 0) {
+            mpn_add_n(sum, sum, term, N);
+        } else {
+            mpn_sub_n(sum, sum, term, N);
+        }
+    }
+    
+    mpz_t pi, z_sum, den;
+    mpz_inits(pi, z_sum, den, NULL);
+
+    mpz_set_ui(den, 10005);
+    mpz_mul_2exp(den, den, 2 * 64 * N);
+    mpz_sqrt(den, den); 
+
+    mpz_roinit_n(z_sum, sum, N);
+    mpz_mul(den, z_sum, den);
+
+
+    mpz_set_ui(z_sum, 10);
+    mpz_pow_ui(z_sum, z_sum, digits);
+    mpz_mul_ui(z_sum, z_sum, 4270934400ULL);
+    mpz_mul_2exp(z_sum, z_sum, 64 * (2 * N - 1));
+
+    mpz_tdiv_q(pi, z_sum, den);
+
+    char *str = mpz_get_str(NULL, 10, pi);
+    if (str) {
+        printf("pi: %c.%s\n", str[0], str + 1);
+        free(str);
     }
 
-    mpz_t z_sum;
-    mpz_roinit_n(z_sum, sum, N);
-
-    mpf_t f_sum;
-    mpf_init2(f_sum, bits);
-    mpf_set_z(f_sum, z_sum);
-    mpf_div_2exp(f_sum, f_sum, 64 * (N - 1));
-
-    mpf_t constant;
-    mpf_init2(constant, bits);
-    mpf_sqrt_ui(constant, 10005);
-    mpf_div_ui(constant, constant, 4270934400);
-    mpf_mul(f_sum, f_sum, constant);
-    mpf_clear(constant);
-
-    mpf_t pi;
-    mpf_init2(pi, bits);
-    mpf_ui_div(pi, 1, f_sum);
-    mpf_clear(f_sum);
-
-    //1 extra porque a desgraça me arredonda
-    char pi_size = 1 + digits + sizeof("3."); // 3.{$digits}\0
-    char pi_str[pi_size]; 
-    gmp_snprintf(pi_str, pi_size, "%.*Ff", digits + 1, pi);
-    mpf_clear(pi);
-
-    pi_str[pi_size - 2] = '\0';
-    printf("pi: %s\n", pi_str);
+    mpz_clears(den, z_sum, pi, NULL);
 }
-
 
 int main(int argc, char** argv) {
     int digits = argc > 1 ? atoi(argv[1]) : 1000;
