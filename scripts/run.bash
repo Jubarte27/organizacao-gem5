6a67109d1f073d6ae4779d469743269ddd87ebf6
Jubarte <mmcezar@inf.ufrgs.br>
2026-06-15 07:47:22 -0300

    generalized run

--- scripts/run.bash
+++ scripts/run.bash
@@ -6,33 +6,25 @@ animals=("panda" "koala" "otter" "fox" "badger" "hedgehog" "dolphin" "penguin" "
 
 main() {
     set_log_depth 0
-
-    run_adj=${adjectives[$RANDOM % ${#adjectives[@]}]}
-    run_anim=${animals[$RANDOM % ${#animals[@]}]}
-    run_uuid=$(cat /proc/sys/kernel/random/uuid)
-
-    local docker_dir="/data"
-
-    local run_dir=".run/$run_adj-$run_anim-$run_uuid"
-    mkdir -p "$run_dir"
-    echo i live on "$run_dir"
+    ensure cd "$PROJECT_DIR"
 
     local build_dir="src/build"
     mkdir -p "$build_dir"
 
+    local gem5=("${EXEC_PREFIX[@]}" "$EXEC")
+    local compile_exec=("${EXEC_PREFIX[@]}" "$TARGET_DIR/compile.sh")
+
     python3 "$PROJECT_DIR/src/make_data.py" 8 10
-    cp "$PROJECT_DIR/src/big_array.h" "$run_dir/radix.big_array.h"
-    cd "$PROJECT_DIR" && run_docker ./compile.sh "$docker_dir/$run_dir"
-    EXEC="$GEM_5_DIR/build/X86/gem5.opt"
+    cp "$PROJECT_DIR/src/big_array.h" "$HOST_RUN_DIR/radix.big_array.h"
+    "${compile_exec[@]}" "$TARGET_RUN_DIR"
 
-    cp -r orgb_configs "$run_dir/orgb_configs"
-    cd "$PROJECT_DIR/$run_dir" || exit 1
+    cp -r "$PROJECT_DIR/orgb_configs" "$HOST_RUN_DIR/orgb_configs"
+    cp "$PROJECT_DIR/src/cha.c" "$HOST_RUN_DIR/cha.in"
 
-    cp "$PROJECT_DIR/src/cha.c" "$PROJECT_DIR/$run_dir/cha.in"
-
-    run_docker $EXEC --outdir=chud.m5out orgb_configs/simulate.py run-benchmark -c chud -o "100" 2>&1 | tee "$PROJECT_DIR/$run_dir/chud.txt"
-    run_docker $EXEC --outdir=radix.m5out orgb_configs/simulate.py run-benchmark -c radix 2>&1 | tee "$PROJECT_DIR/$run_dir/radix.txt"
-    run_docker $EXEC --outdir=cha.m5out orgb_configs/simulate.py run-benchmark -c cha 2>&1 | tee "$PROJECT_DIR/$run_dir/cha.txt"
+    ensure cd "$HOST_RUN_DIR"
+    "${gem5[@]}" --outdir=chud.m5out orgb_configs/simulate.py run-benchmark -c chud -o "100" 2>&1 | tee "chud.txt"
+    "${gem5[@]}" --outdir=radix.m5out orgb_configs/simulate.py run-benchmark -c radix 2>&1 | tee "radix.txt"
+    "${gem5[@]}" --outdir=cha.m5out orgb_configs/simulate.py run-benchmark -c cha 2>&1 | tee "cha.txt"
 }
 
 build_if_not_exists() {
@@ -66,10 +58,39 @@ _setConfigArgs() {
     done
 }
 
+docker_exists() {
+    {
+        { docker image ls --filter reference="$IMAGE_NAME" --format "{{.Repository}}:{{.Tag}}" | grep --line-regexp -q "$IMAGE_NAME"; } > /dev/null 2>&1
+    } && { run_docker [ -f "$DOCKER_GEM5" ]; }
+}
 
 set_env() {
     IMAGE_NAME=orgb:latest
-    GEM_5_DIR=/opt/gem5
+    DOCKER_GEM5="/opt/gem5/build/X86/gem5.opt"
+    VM_GEM5="/home/orgb/gem5/build/X86/gem5.opt"
+    EXEC_PREFIX=()
+
+    run_adj=${adjectives[$RANDOM % ${#adjectives[@]}]}
+    run_anim=${animals[$RANDOM % ${#animals[@]}]}
+    run_uuid=$(cat /proc/sys/kernel/random/uuid)
+
+    RUN_DIR=".run/$run_adj-$run_anim-$run_uuid"
+    HOST_RUN_DIR="$PROJECT_DIR/$RUN_DIR"
+    mkdir -p "$RUN_DIR"
+    echo i live on "$RUN_DIR"
+
+    if [ -f "$VM_GEM5" ] ; then
+        EXEC="$VM_GEM5"
+        TARGET_DIR="$PROJECT_DIR"
+        TARGET_RUN_DIR="$HOST_RUN_DIR"
+    elif docker_exists; then
+        EXEC="$DOCKER_GEM5"
+        TARGET_DIR="/data"
+        TARGET_RUN_DIR="/data/$RUN_DIR"
+        EXEC_PREFIX+=("run_docker")
+    else
+        log_error "Couldn't find gem5.opt"
+    fi
 }
 
 SCRIPT_DIR=$(dirname "$(readlink -e "${BASH_SOURCE[0]}")") && source "$SCRIPT_DIR/util.bash"
